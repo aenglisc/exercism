911c8c24cdeeb52cb25f37d17a07ee5eb903d436
Roman Pushkov <pushkovroman@me.com>
2017-08-05 15:11:24 +0300

    added perfect check

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -1,11 +1,39 @@
-const isNatural = num => num >= 1 && num % 1 === 0;
+// is num a natural number?
+const notNatural = num => !(num >= 1 && num % 1 === 0);
 
+// is num even?
+const isEven = num => num % 2 === 0;
+
+// is num even and perfect?
+// no odd perfect numbers have been found, so this might be redundant
+const isEvenPerfect = (num) => {
+  if (isEven(num)) {
+    const numBinary = Number(num).toString(2);
+
+    if (!isEven(numBinary.length)) {
+      const chunkOne = numBinary.slice(0, -numBinary.length / 2);
+      const chunkTwo = numBinary.slice(-numBinary.length / 2);
+
+      if (!chunkOne.includes(0) && !chunkTwo.includes(1)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+// in progress
+const isOddAbundant = (num) => {
+  if (num < 945) { return false; }
+  return true;
+};
+
+// is num a prime?
 const isPrime = (num) => {
   if (num === 2) { return true; }
+  if (num <= 1 || isEven(num)) { return false; }
 
   const sqrtNum = Math.sqrt(num);
-  if (num <= 1 || num % 2 === 0) { return false; }
-
   const iterCheck = (div) => {
     if (div > sqrtNum) { return true; }
     if (num % div === 0) { return false; }
@@ -15,12 +43,17 @@ const isPrime = (num) => {
   return iterCheck(3);
 };
 
-const notNatErr = 'Classification is only possible for natural numbers.';
+// error contents
+const notNatErrText = 'Classification is only possible for natural numbers.';
 
+// solution
 export default class PerfectNumbers {
   classify(num) {
-    if (!isNatural(num)) { throw new Error(notNatErr); }
+    if (notNatural(num)) { throw new Error(notNatErrText); }
     if (isPrime(num)) { return 'deficient'; }
+    if (isEvenPerfect(num)) { return 'perfect'; }
+    // in progress
+    if (isOddAbundant(num)) { return 'abundant'; }
 
     const iterClassify = (div, sum) => {
       if (num === div) {
